0f52c620e1b47570f176dff363f0671d9305e5e3
aorrego <aorrego@xumak.com>
2014-12-25 17:28:06 -0500

    Added Satellizer

--- client/app.js
+++ client/app.js
@@ -3,11 +3,12 @@
   angular
     .module('Instagram', [
       'ngRoute',
-      'ngMessages'
+      'ngMessages',
+      'satellizer'
     ])
     .config(config);
 
-  function config($routeProvider) {
+  function config($routeProvider, $authProvider) {
     $routeProvider
       .when('/', {
         templateUrl: 'views/home.html',
@@ -26,6 +27,22 @@
         controller: 'DetailCtrl'
       })
       .otherwise('/');
+
+
+    $authProvider.loginUrl = 'http://localhost:3000/auth/login';
+    $authProvider.signupUrl = 'http://localhost:3000/auth/signup';
+    $authProvider.oauth2({
+      name: 'instagram',
+      url: 'http://localhost:3000/auth/instagram',
+      redirectUri: 'http://localhost:8000',
+      clientId: '799d1f8ea0e44ac8b70e7f18fcacedd1',
+      requiredUrlParams: ['scope'],
+      scope: ['likes'],
+      scopeDelimiter: '+',
+      authorizationEndpoint: 'https://api.instagram.com/oauth/authorize'
+    });
+
+
   }
 
 })();
