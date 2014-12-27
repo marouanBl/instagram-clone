383db7522482dc176a39731a8f71b8b3a97a5861
aorrego <aorrego@xumak.com>
2014-12-27 11:44:39 -0500

    Added login route

--- server/server.js
+++ server/server.js
@@ -30,6 +30,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+/**
+ * Creates a new JWT encode based on a user
+ * @param user
+ * @returns {String|*}
+ */
 function createToken(user) {
   var payload = {
     exp: moment().add(14, 'days').unix(),
@@ -40,7 +45,57 @@ function createToken(user) {
   return jwt.encode(payload, config.tokenSecret);
 }
 
+/**
+ * Verifies if a user is authenticated using JWT Token
+ *
+ * @param req
+ * @param res
+ * @param next
+ * @returns {*}
+ */
+function isAuthenticated(req, res, next) {
+  if (!(req.headers && req.headers.authorization))
+    return res.status(400).send({ message: 'You did not provide a JSON Web Token in the Authorization header.' });
+
+  var header = req.headers.authorization.split(' ');
+  var token = header[1];
+  var payload = jwt.decode(token, config.tokenSecret);
+  var now = moment().unix();
+
+  if (now > payload.exp)
+    return res.status(401).send({message: 'Token has expired'});
+
+  User.findById(payload.sub, function (err, user) {
+    if (!user)
+      return res.status(400).send({message: 'User no longer exists.'});
+
+    req.user = user;
+
+    next();
+  });
+
+}
+
+app.post('/auth/login', function (req, res) {
+  User.findOne({ email: req.body.email }, '+password', function (err, user) {
+    if (!user)
+      return res.status(401).send({message: {email: 'Incorrect email'}});
+    
+    bcrypt.compare(req.body.password, user.password, function (err, isMatch) {
+      if (!isMatch)
+        return res.status(401).send({message: {password: 'Incorrect password'}});
+
+      user = user.toObject();
+      delete user.password;
+
+      var token = createToken(user);
+      res.send({token: token, user: user});
+    });
+  });
+});
+
 
 app.listen(app.get('port'), function () {
+  console.log(middleware);
   console.log('Express server listening on port', app.get('port'));
 });
