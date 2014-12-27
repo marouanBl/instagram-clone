d5864d0ae2b55e3c8fafb9cfbf21848b543a222b
aorrego <aorrego@xumak.com>
2014-12-27 11:57:04 -0500

    Added login and signup(email)

--- server/server.js
+++ server/server.js
@@ -63,11 +63,11 @@ function isAuthenticated(req, res, next) {
   var now = moment().unix();
 
   if (now > payload.exp)
-    return res.status(401).send({message: 'Token has expired'});
+    return res.status(401).send({ message: 'Token has expired' });
 
   User.findById(payload.sub, function (err, user) {
     if (!user)
-      return res.status(400).send({message: 'User no longer exists.'});
+      return res.status(400).send({ message: 'User no longer exists.' });
 
     req.user = user;
 
@@ -76,26 +76,48 @@ function isAuthenticated(req, res, next) {
 
 }
 
+app.post('/auth/signup', function (req, res) {
+  User.findOne({ email: req.body.email }, function (err, existingUser) {
+    if (existingUser)
+      return res.status(409).send({ message: 'Email is already taken' });
+
+    var user = new User({
+      email: req.body.email,
+      password: req.body.password
+    });
+
+    bcrypt.getSalt(10, function (err, salt) {
+      bcrypt.hash(user.password, salt, function (err, hash) {
+        user.password = hash;
+
+        user.save(function () {
+          var token = createToken(user);
+          res.send({ token: token, user: user });
+        });
+      });
+    });
+
+  });
+});
+
 app.post('/auth/login', function (req, res) {
   User.findOne({ email: req.body.email }, '+password', function (err, user) {
     if (!user)
-      return res.status(401).send({message: {email: 'Incorrect email'}});
-    
+      return res.status(401).send({ message: { email: 'Incorrect email' } });
+
     bcrypt.compare(req.body.password, user.password, function (err, isMatch) {
       if (!isMatch)
-        return res.status(401).send({message: {password: 'Incorrect password'}});
+        return res.status(401).send({ message: { password: 'Incorrect password' } });
 
       user = user.toObject();
       delete user.password;
 
       var token = createToken(user);
-      res.send({token: token, user: user});
+      res.send({ token: token, user: user });
     });
   });
 });
 
-
 app.listen(app.get('port'), function () {
-  console.log(middleware);
   console.log('Express server listening on port', app.get('port'));
 });
